739358837583a82483e834eca8b78d19cb4c9074
RPG-Alex <mralexblood@outlook.com>
2026-08-27 12:57:26 +0800

    added proposed field for mass tracking

--- src/records.rs
+++ src/records.rs
@@ -3,5 +3,6 @@ use std::collections::BTreeMap;
 #[derive(Debug, Clone)]
 pub(crate) struct MoleculeRecord {
     pub(crate) element_counts: BTreeMap<String, usize>,
+    pub(crate) monoisotopic_mass_da: f64,
     pub(crate) metadata: BTreeMap<String, String>,
 }
